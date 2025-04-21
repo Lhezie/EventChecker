84a43d3176871828b2aaab991349080cbd66cd19
Elizabeth Enechaziam <lizabethbunor@gmail.com>
2025-04-21 18:56:55 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,11 @@ import app from './app.js';
 const PORT = process.env.PORT || 3020;
 
 
+app.use(cors({
+  origin: "https://event-checker-one.vercel.app",
+}));
+app.use(express.json());
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
